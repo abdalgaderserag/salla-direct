6ddbd8db1a381bc9698a58ea67b838904f5a5b36
Abd Al-Gader Sirag <abdalgader.dev@gmail.com>
2025-03-01 04:14:03 -0800

    start adding loal

---
+++ lang/en/views.php
@@ -0,0 +1,64 @@
+<?php
+return [
+
+    // Layouts
+    'layout.nav.tutorials' => 'Tutorials',
+    'layout.nav.topics' => 'Topics',
+    'layout.nav.connected' => 'Connected',
+    'layout.nav.logout' => 'Logout',
+    'layout.nav.logo' => '/svg/logo.svg',
+    'layout.nav.logo.dark' => '/svg/logo-dark.svg',
+
+    'layout.side.search' => 'search',
+
+    'layout.side.clients' => 'clients',
+    'layout.side.clients.list' => 'Client List',
+    'layout.side.clients.ban' => 'Banned List',
+    'layout.side.clients.campaign' => 'Campaigns',
+
+    'layout.side.content' => 'content',
+    'layout.side.content.whatsapp' => 'Whatsapp Widget',
+    'layout.side.content.auto' => 'Auto Messages',
+    'layout.side.content.bounces' => 'Bounces',
+
+    'layout.side.settings' => 'settings',
+    'layout.side.settings.whatsapp' => 'Whatsapp',
+    'layout.side.settings.store' => 'Store',
+
+    // client list
+    'list.header' => 'Customers',
+    'list.header.count' => 'client',
+    'list.header.details' => 'These are all the customers that are aligable to send campaigns to.',
+    'list.header.export' => 'Export',
+    'list.header.add' => 'Add Customer',
+
+    'list.button.groups' => 'Groups',
+
+    'list.table.head.name' => 'Name',
+    'list.table.head.groups' => 'Groups',
+    'list.table.head.info' => 'Info',
+    'list.table.head.city' => 'City',
+    'list.table.head.register' => 'Register At',
+    'list.table.footer.page' => 'Page',
+    'list.table.footer.of' => 'of',
+    'list.table.footer.next' => 'Next',
+    'list.table.footer.back' => 'Previous',
+
+    'list.form.title' => 'Account settings',
+    'list.form.name.first' => 'First Name',
+    'list.form.name.last' => 'Last Name',
+    'list.form.email' => 'Email Address',
+    'list.form.birthday' => 'Birth Day',
+    'list.form.gender' => 'Gender',
+    'list.form.gender.place' => 'Select gender',
+    'list.form.gender.male' => 'Male',
+    'list.form.gender.female' => 'Female',
+
+    'list.form.phone' => 'Phone Number',
+    'list.form.phone.code' => 'Code',
+    'list.form.phone.place' => 'Select Country code',
+
+    'list.form.save' => 'Save',
+
+
+];
